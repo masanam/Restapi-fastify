f06a9e86c675309b67d4f54e59b897dbc0643767
Choirul Anam <choirul.anam@redtech.co.id>
2020-10-06 17:54:05 +0700

    update

--- app.js
+++
@@ -1,8 +0,0 @@
-const fastify = require('fastify')({
-    logger: true //aktifkan ini untuk menerima log setiap request dari fastify.
-});
-const routes = require('./routes')
-fastify.register(routes);
-fastify.register(require('fastify-formbody'));
-
-module.exports = fastify
