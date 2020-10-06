58b900d64c6ca9c28f33b8b30f4cb0953fc61487
Choirul Anam <choirul.anam@redtech.co.id>
2020-10-06 17:55:24 +0700

    update

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict'
+require('dotenv').config();
+const fastify = require('./app')
+
+const start = async () => {
+    try {
+        await fastify.listen(process.env.APP_PORT || 3000);
+
+        fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1)
+    }
+};
+
+start();
